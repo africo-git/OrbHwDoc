ddccf941ded05d632cbdc3d42e7ce50de5cc0cb2
africo-git <89541827+africo-git@users.noreply.github.com>
2021-08-27 16:20:11 +0200

    Creación UC Básico Control Versiones
    Orientado a Issue #5.

---
+++ DocVerCtrl_Uc.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace OrbHwDoc
+{
+    partial class DocVerCtrl_Uc
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.myDocVerCtrl_Uc_Wpf = new OrbHwDoc.DocVerCtrl_Uc_Wpf();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.elementHost1.Location = new System.Drawing.Point(0, 0);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(590, 400);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.myDocVerCtrl_Uc_Wpf;
+            // 
+            // DocVerCtrl_Uc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.elementHost1);
+            this.Name = "DocVerCtrl_Uc";
+            this.Size = new System.Drawing.Size(590, 400);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private DocVerCtrl_Uc_Wpf myDocVerCtrl_Uc_Wpf;
+    }
+}
