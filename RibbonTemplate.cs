f2fd3c039267bb4cf31651a7cf2ef670bc15c33d
africo-git <89541827+africo-git@users.noreply.github.com>
2021-08-31 10:06:15 +0200

    Incluido botón y apertura y panel

--- RibbonTemplate.cs
+++ RibbonTemplate.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Windows.Forms;
+
+
 namespace OrbHwDoc
 {
     public partial class RibbonTemplate
@@ -18,15 +21,37 @@ namespace OrbHwDoc
         {
             if(TglBtoDocIdProp.Checked)
             {
+                Globals.ThisDocument.ActionsPane.Controls.Remove(ThisDocument.MyDocIdProp_Uc);
                 Globals.ThisDocument.ActionsPane.Controls.Add(ThisDocument.MyDocIdProp_Uc);
                 Globals.ThisDocument.Application.TaskPanes[Word.WdTaskPanes.wdTaskPaneDocumentActions].Visible = true;
 
-                ThisDocument.MyDocIdProp_Uc.UpdateFrom();
+                TglBtoCtrlVer.Enabled = false;  // Desactivado otros botones incompatibles
             }
             else
             {
                 Globals.ThisDocument.ActionsPane.Controls.Remove(ThisDocument.MyDocIdProp_Uc);
                 Globals.ThisDocument.Application.TaskPanes[Word.WdTaskPanes.wdTaskPaneDocumentActions].Visible = false;
+
+                TglBtoCtrlVer.Enabled = true;   // Activa los botones que eran incompatibles
+            }
+        }
+
+        private void TglBtoCtrlVer_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (TglBtoCtrlVer.Checked)
+            {
+                Globals.ThisDocument.ActionsPane.Controls.Remove(ThisDocument.MyDocIdProp_Uc);
+                Globals.ThisDocument.ActionsPane.Controls.Add(new Button());    // Probamos con un botón
+                Globals.ThisDocument.Application.TaskPanes[Word.WdTaskPanes.wdTaskPaneDocumentActions].Visible = true;
+
+                TglBtoDocIdProp.Enabled = false;    // Desactivado otros botones incompatibles
+            }
+            else
+            {
+                Globals.ThisDocument.ActionsPane.Controls.Remove(ThisDocument.MyDocIdProp_Uc);
+                Globals.ThisDocument.Application.TaskPanes[Word.WdTaskPanes.wdTaskPaneDocumentActions].Visible = false;
+
+                TglBtoDocIdProp.Enabled = true;     // Activa los botones que eran incompatibles
             }
         }
     }
